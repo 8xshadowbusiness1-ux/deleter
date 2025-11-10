8f9e5ddedca6f8732309ac40f2b7a7a74abeadbe
8xshadowbusiness1-ux <8xshadowbusiness1@gmail.com>
2025-11-10 20:45:53 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "🚀 Starting Auto Deleter Bot..."
+
+# Install dependencies (optional if you already installed manually)
+pip install -r requirements.txt
+
+# Start the bot
+python3 deleter.py
